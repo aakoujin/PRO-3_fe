74615b5586b24b77d387ba126416648e9d5bd692
Artem Andreoli <artemandreoli@gmail.com>
2022-11-22 23:15:26 +0200

    initial commit to refactoring FE from js to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const result =
+        await fetch("http://localhost:42999/api/User",
+          {
+            headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+            method: 'GET'
+          })
+      const users = await result.json()
+      console.log(users.$values)
+
+      setUsers(users.$values)
+    }
+
+    fetchUsers();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='userContainer'>
+      {users.map(user =>
+        <div>{JSON.stringify(user)}</div>
+      )}
     </div>
   );
 }
