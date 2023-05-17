d286154402e3974f86f16756576157d19085c5f3
Artem Andreoli <artemandreoli@gmail.com>
2023-05-18 00:20:44 +0300

    -added token persistance in global state

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './context/AuthProvider';
+//import { AuthProvider } from './context/AuthProvider';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
 
