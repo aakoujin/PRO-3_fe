e86c622b75635b8df6741da87d93939cacdb540a
Artem Andreoli <artemandreoli@gmail.com>
2023-08-06 16:30:15 +0300

    migrated listing cards to MUI

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -1,7 +1,13 @@
 import { useContext } from "react";
-import { Card, CardImg } from "react-bootstrap";
+//import { Card, CardImg } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthProvider";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 export interface ListingItem {
     id_listing: number;
@@ -37,13 +43,31 @@ function Listing(listing: ListingItem) {
 
     return (
         <>
-            <Card onClick={handleClick}>
-                <CardImg variant="top" height="200px" style={{ objectFit: "cover" }} src={displayableMedia.$values[0].media} alt="placeholder" />
-                <div className='listing'>
+            
+            <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  onClick={handleClick}
+                >
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image={displayableMedia.$values[0].media}
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
                     {listing.post_name}
+                    </Typography>
+                    <Typography>
+                    {listing.post_desc}
+                    </Typography>
+                    <Typography>
                     {listing.price}
-                </div>
-            </Card>
+                    </Typography>
+                  </CardContent>
+                </Card>
         </>
     )
 
@@ -51,3 +75,13 @@ function Listing(listing: ListingItem) {
 
 export default Listing
 //add handling for no-image case
+//add edit functionality
+
+
+/*<Card onClick={handleClick}>
+                <CardImg variant="top" height="200px" style={{ objectFit: "cover" }} src={displayableMedia.$values[0].media} alt="placeholder" />
+                <div className='listing'>
+                    {listing.post_name}
+                    {listing.price}
+                </div>
+            </Card>*/
